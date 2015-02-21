619874161d3a920bf6cff163ac1fbee4a088ada8
Joe <joe_mason@rocketmail.com>
2015-02-21 23:46:39 +0000

    stuff

---
+++ App_Code/WebServiceAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ExamServer;
+
+/// <summary>
+/// Summary description for WebServiceAccess
+/// </summary>
+public class WebServiceAccess
+{
+	public WebServiceAccess()
+	{
+
+
+	}
+
+    public bool Login()
+    {
+        var temp = new ExamServer.WebService();
+        temp.StudentLogin()
+
+
+
+    }
+}
